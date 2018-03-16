4d634122d7d821fc950aa7aa1360760db9cb31f2
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-16 03:17:01 -0700

    saravana16 2(3).c

---
+++ saravana16 2(3).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ float a;
+ printf("enter the floating point number:");
+ scanf("%f",&a);
+ printf("the round of floating point %f is %f",a,round(a));
+ getch();
+ return 0;
+ }
