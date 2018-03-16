2df8a08a68dd25a04b18c139f7b9fdebc905c778
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-16 03:11:34 -0700

    saravana 16 1(3).c

---
+++ saravana 16 1(3).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n=2,m=4,o;
+printf("\n Enter o:");
+scanf("%d",&o);
+if(n<o<m)
+{
+printf("\nYES");
+}
+else
+{
+printf("\nNO");
+}
+getch();
+return 0;
+}
